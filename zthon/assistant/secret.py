f057977e0328585a92fcb8792043b09561770af4
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-03-13 21:44:15 +0300

    Update secret.py

--- zthon/assistant/secret.py
+++ zthon/assistant/secret.py
@@ -27,9 +27,9 @@ async def on_plug_in_callback_query_handler(event):
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
-                reply_pop_up_alert = "مطـي الهمسـه مـو الك 🧑🏻‍🦯🦓"
+                reply_pop_up_alert = "کەرە ئەمە بۆتۆ نییە 🧑🏻‍🦯🦓"
         except KeyError:
-            reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+            reply_pop_up_alert = "- ببورە .. ئەم نامەیە لە سێرڤەری بۆتی زیرەك نییە "
     else:
-        reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
+        reply_pop_up_alert = "- ببوورە .. ئەم نامەیە لە سێرڤەری بۆتی زیرەك نییە "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
